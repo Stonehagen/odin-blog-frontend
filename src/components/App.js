7b6ac0fea3b88bde716f174c5bc6f15a8189d693
Stonehagen <steinhagen@mailbox.org>
2023-03-22 16:49:00 +0100

    add PostDetail Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import { useCookies } from 'react-cookie';
 import { setAuthToken } from '../methods/setAuthToken';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import '../styles/App.css';
 
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 import CreatePost from './CreatePost';
 import Posts from './Posts';
-
-import '../styles/App.css';
+import PostDetail from './PostDetail';
 
 const App = () => {
   const [cookies] = useCookies(['jwt_token']);
@@ -21,6 +21,7 @@ const App = () => {
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/posts" element={<Posts />} />
+        <Route path="/post/:id" element={<PostDetail />} />  
         <Route path="/post/new" element={<CreatePost />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
---
+++ src/components/PostDetail.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import '../styles/PostDetail.css';
+
+const PostDetail = () => {
+  const [post, setPost] = useState([]);
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    /// implement dotenv api url !!!
+    axios
+      .get(`http://localhost:3000/post/${id}/`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        if (res.data.error) {
+          return res.data.error;
+        } else {
+          setPost(res.data.post);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
+  return (
+    <div className="Post">
+      <h2>{post.title}</h2>
+      <p>{post.text}</p>
+      <p>{post.timestamp}</p>
+    </div>
+  );
+};
+
+export default PostDetail;
