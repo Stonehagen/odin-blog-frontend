1b9674a2f0bf4b50b1b01f0c464e46819b080e89
Stonehagen <steinhagen@mailbox.org>
2023-04-07 22:05:16 +0200

    change homepage in package.json

--- src/methods/loadImage.js
+++ src/methods/loadImage.js
@@ -3,6 +3,6 @@ export const loadImage = (fileName) => {
   let src = `../img/${fileName}.jpg`;
   image.src = src;
   return image.width === 0
-    ? `/img/default.jpg`
-    : `/img/${fileName}.jpg`;
+    ? `../img/default.jpg`
+    : `../img/${fileName}.jpg`;
 };
