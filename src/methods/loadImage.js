3adf4dee20a16b2f4f234eb009e65ff838630b43
Stonehagen <steinhagen@mailbox.org>
2023-04-07 21:52:16 +0200

    change relative path

--- src/methods/loadImage.js
+++ src/methods/loadImage.js
@@ -3,6 +3,6 @@ export const loadImage = (fileName) => {
   let src = `../img/${fileName}.jpg`;
   image.src = src;
   return image.width === 0
-    ? `../public/img/default.jpg`
-    : `../public/img/${fileName}.jpg`;
+    ? `/img/default.jpg`
+    : `/img/${fileName}.jpg`;
 };
